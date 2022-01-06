caa59f5f8794f7717f8e29df9164a957bdb17f8f
lalalademaxiya1 <66767061+lalalademaxiya1@users.noreply.github.com>
2022-01-06 11:01:24 +0800

    Update q0 and q7.

--- keyboards/keychron/q7/rev_0170/rev_0170.c
+++ keyboards/keychron/q7/rev_0170/rev_0170.c
@@ -17,7 +17,6 @@
 #include "quantum.h"
 
 #ifdef RGB_MATRIX_ENABLE
-
 const ckled2001_led PROGMEM g_ckled2001_leds[DRIVER_LED_TOTAL] = {
 /* Refer to CKLED manual for these locations
  *   driver
@@ -126,5 +125,4 @@ led_config_t g_led_config = {
         4,    4,    4,                      4,                      4,    4,    4,    4,    4,    4,    4,
     }
 };
-
 #endif
