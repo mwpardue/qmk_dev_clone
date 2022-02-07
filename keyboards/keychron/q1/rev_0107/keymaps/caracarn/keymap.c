967fb2cb6d2c89fa479a475f75ec3d7aa14ebc70
mwpardue <mwpardue@gmail.com>
2022-02-06 23:22:01 -0500

    adding TD_LCMD_CAPS to Caps Lock key

--- keyboards/keychron/q1/rev_0107/keymaps/caracarn/keymap.c
+++ keyboards/keychron/q1/rev_0107/keymaps/caracarn/keymap.c
@@ -39,6 +39,18 @@ user_config_t user_config;
     #define USER_START SAFE_RANGE
 #endif
 
+// Tap Dance declarations
+enum {
+    TD_LCMD_CAPS,
+};
+
+// Tap Dance definitions
+qk_tap_dance_action_t tap_dance_actions[] = {
+    // Tap once for Escape, twice for Caps Lock
+    [TD_LCMD_CAPS] = ACTION_TAP_DANCE_DOUBLE(KC_LCMD, KC_CAPS),
+};
+
+
 enum custom_keycodes {
     KC_MISSION_CONTROL = USER_START,
     KC_LAUNCHPAD,
@@ -82,7 +94,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,    KC_F12,  KC_SLEP,             KC_MUTE,
         KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,   KC_EQL,   KC_BSPC,            KC_PGUP,
         KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,   KC_RBRC,  KC_BSLS,            KC_PGDN,
-        KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,             KC_ENT,             KC_DEL,
+        TD_LCMD_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,             KC_ENT,             KC_DEL,
         KC_LSPO,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,             KC_RSPC,  KC_UP,
         KC_LCTL,  KC_LOPTN, KC_LCMMD,                               KC_SPC,   KC_VOLD,  KC_VOLU,            KC_RCMMD,TT(MAC_FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
--- keyboards/keychron/q1/rev_0107/keymaps/caracarn/rules.mk
+++ keyboards/keychron/q1/rev_0107/keymaps/caracarn/rules.mk
@@ -1,5 +1,6 @@
 VIA_ENABLE = yes
 MOUSEKEY_ENABLE = no
+TAP_DANCE_ENABLE = yes
 
 ifeq ($(strip $(RGB_MATRIX_ENABLE)), yes)
     SRC += rgb_matrix_user.c
