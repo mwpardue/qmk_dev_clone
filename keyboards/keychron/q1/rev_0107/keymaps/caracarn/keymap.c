646142e9b96b0375de3f6215209befceec11d57f
mwpardue <mwpardue@gmail.com>
2022-02-21 08:51:14 -0500

    Removing homerow mods

--- keyboards/keychron/q1/rev_0107/keymaps/caracarn/keymap.c
+++ keyboards/keychron/q1/rev_0107/keymaps/caracarn/keymap.c
@@ -63,16 +63,16 @@ key_combination_t key_comb_list[2] = {
 #define SPACNAV LT(NAV, KC_SPC)
 
 // Left-hand home row mods
-#define HOME_A LCTL_T(KC_A)
+/* #define HOME_A LCTL_T(KC_A)
 #define HOME_S LALT_T(KC_S)
 #define HOME_D LSFT_T(KC_D)
-#define HOME_F LGUI_T(KC_F)
+#define HOME_F LGUI_T(KC_F) */
 
 // Right-hand home row mods
-#define HOME_J RGUI_T(KC_J)
+/* #define HOME_J RGUI_T(KC_J)
 #define HOME_K RSFT_T(KC_K)
 #define HOME_L LALT_T(KC_L)
-#define HOME_SCLN RCTL_T(KC_SCLN)
+#define HOME_SCLN RCTL_T(KC_SCLN) */
 
 static uint8_t mac_keycode[4] = { KC_LOPT, KC_ROPT, KC_LCMD, KC_RCMD };
 
@@ -81,7 +81,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,    KC_F1,     KC_F2,     KC_F3,     KC_F4,     KC_F5,     KC_F6,     KC_F7,     KC_F8,     KC_F9,     KC_F10,    KC_F11,    KC_F12,    KC_SLEP,         KC_VOLD, KC_MUTE, KC_VOLU,
         KC_GRV,    KC_1,      KC_2,      KC_3,      KC_4,      KC_5,      KC_6,      KC_7,      KC_8,      KC_9,      KC_0,      KC_MINS,   KC_EQL,    KC_BSPC,                  KC_PGUP,
         KC_TAB,    KC_Q,      KC_W,      KC_E,      KC_R,      KC_T,      KC_Y,      KC_U,      KC_I,      KC_O,      KC_P,      KC_LBRC,   KC_RBRC,   KC_BSLS,                  KC_PGDN,
-        CAPSFN,    HOME_A,    HOME_S,    HOME_D,    HOME_F,    KC_G,      KC_H,      HOME_J,    HOME_K,    HOME_L,    HOME_SCLN, KC_QUOT,              KC_ENT,                   KC_DEL,
+        CAPSFN,    KC_A,      KC_S,      KC_D,      KC_F,      KC_G,      KC_H,      KC_J,      KC_K,      KC_L,      KC_SCLN,   KC_QUOT,              KC_ENT,                   KC_DEL,
         KC_LSPO,              KC_Z,      KC_X,      KC_C,      KC_V,      KC_B,      KC_N,      KC_M,      KC_COMM,   KC_DOT,    KC_SLSH,              KC_RSPC,       KC_UP,
         KC_LCTL,   KC_LOPTN,  KC_LCMMD,                                   SPACNAV,                                    KC_RCMMD,  TTMACFN,   KC_RCTL,       KC_LEFT,   KC_DOWN,   KC_RGHT),
 
@@ -234,7 +234,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 }
 
 
-bool get_tapping_force_hold(uint16_t keycode, keyrecord_t *record) {
+/* bool get_tapping_force_hold(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case LT(1, KC_BSPC):
         case LCTL_T(KC_A):
@@ -249,4 +249,4 @@ bool get_tapping_force_hold(uint16_t keycode, keyrecord_t *record) {
         default:
             return false;
     }
-}
+} */
